00b2174606ad1a244acc4b070bbc0df37dea6a4a
f1era <workf1era@gmail.com>
2018-03-04 13:57:13 +0300

    13:57 4.02.18

---
+++ js/script.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+
+	/*$('.offer21').slideToggle();*/
+	$('.reg_block').hide();
+	$('.login_block').hide();
+
+
+	regblock = false;
+	loginblock = false;
+
+	$('.login_b1').click(function() {
+		if(loginblock == true) {
+			loginblock = false;
+			$('.login_block').animate({
+				left: '-=100%',
+			}, 700, function() {
+				$('.login_block').hide();
+				
+			});
+		}
+	});
+	
+
+	$('.enter_btn').click(function(){
+		if(regblock == false) {
+			if(loginblock == false) {
+				loginblock = true;
+				$('.login_block').show();
+				$('.login_block').animate({
+					left: '+=100%',
+				}, 700, function() {
+					
+				});
+			}
+		}
+	});
+
+
+
+
+	$('.reg_b1').click(function() {
+		if(regblock == true) {
+			regblock = false;
+			$('.reg_block').animate({
+				left: '-=100%',
+			}, 700, function() {
+				$('.reg_block').hide();
+				regblock = false;
+			});
+		}
+	});
+	
+	$('.reg_btn').click(function(){
+		if(loginblock == false) {
+			if(regblock == false) {
+				regblock = true;
+				$('.reg_block').show();
+				$('.reg_block').animate({
+					left: '+=100%',
+				}, 700, function() {
+				});
+			}
+		}
+	});
+
+
+
+
+	$('.header_block').click(function(){
+		alert(regblock);
+		alert(loginblock);
+	});
+
+});
