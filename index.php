e8d654cc05745c913c4cece032301c995e92f7ca
f1era <workf1era@gmail.com>
2018-03-06 20:11:27 +0300

    20:11 6.03.18

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/media_style.css">
+
+    <script src="js/script.js"></script>
+
+    <title>Patrol is coming!</title>
+  </head>
+  <body>
+
+   <div class="index_div">
+     
+    <div class="header_block">
+      <div class="header_text">
+        <ul>
+          <li><a href="#">О проекте</a></li>
+          <li><a href="#">Контакты</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="main_block">
+      <div class="main_img">
+        
+      </div>
+      <div class="main_text">
+        <div class="main_text_text">
+          <div class="main_text_text_header">
+            <p>Патруль рядом. Аккуратнее.</p>
+          </div>
+          <div class="main_text_text_text">
+            <p>Смотри на карте, где патруль.</p>
+            <p>Добавляй на карту их местонахождение.</p>
+            <p>Аккуратнее. И помоги другим.</p>
+          </div>
+        </div>
+        <div class="main_btn">
+          <form method="post">
+          <span class="reg_btn">
+            <button>Регистрация</button>
+          </span>
+          <span class="login_btn">
+            <button>Вход</button>
+          </span>
+        </form>
+        </div>
+      </div>
+    </div>
+    <div class="footer_block">
+      @f1era
+    </div>
+
+   </div>
+
+    <!-- Script -->
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+    <script src="js/jq.min.js"></script>
+    <script src="js/script.js"></script>
+
+  </body>
+</html>
